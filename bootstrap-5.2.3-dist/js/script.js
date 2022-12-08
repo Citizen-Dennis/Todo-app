4e20e857a49e92bc94ff61b02666ead5461f1f80
Den <appahdennisamoakoadb@gmail.com>
2022-12-08 15:38:28 +0000

    Added a clear function to the script

--- bootstrap-5.2.3-dist/js/script.js
+++ bootstrap-5.2.3-dist/js/script.js
@@ -1,1 +1,14 @@
+let addbtn = document.getElementById('addbtn');
+let clearbtn = document.getElementById('clearbtn');
+let todo_ol = document.getElementById('mylist');
+
+var todolistarray = [];
+
+var clearlist = function(){
+    todolistarray = []
+    localStorage.removeItem('mylist');
+    myList.innerHTML = '';
+}
+
+clearbtn.addEventListener('click', clearlist);
 
